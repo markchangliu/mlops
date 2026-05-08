c69667cbf992a84e898c8ad8a19febd963c67995
Liu Chang <liu67511717@126.com>
2026-05-08 15:37:01 +0800

    feats (FEAT-01-02): update the codes of labelme -> schema dataset

--- src/mlops/convert/labelme.py
+++ src/mlops/convert/labelme.py
@@ -3,19 +3,21 @@ import os
 from pathlib import Path
 from typing import Literal, Union
 
-from mlops.core.schema import InstancesType
-from mlops.core.schema import LabelmeFileType, LabelmeShapeType
-from mlops.core.schema import OnlineDatasetType, OfflineDatasetType
+import numpy as np
+
+import mlops.convert.shapes as shapes_lib
+import mlops.core.instances as insts_lib
+import mlops.core.schema as schema_lib
 
 
 def _load_labelme_offline(
     img_root: str,
     labelme_root: str,
     cat_name_id_dict: dict[str, int],
-    label_format: Literal["bbox", "mask", "poly"],
+    label_format: Literal["bbox", "poly"],
     add_empty_img_flag: bool
-) -> OfflineDatasetType:
-    dataset_dict: OfflineDatasetType = {
+) -> schema_lib.OfflineDatasetType:
+    dataset_dict: schema_lib.OfflineDatasetType = {
         "cat_id_name_dict": {v:k for k, v in cat_name_id_dict.items()},
         "cat_name_id_dict": cat_name_id_dict,
         "format": label_format,
@@ -45,29 +47,52 @@ def _load_labelme_offline(
 def _labelmeFile2instances(
     label_p: str,
     cat_name_id_dict: dict[str, int],
-    label_format: Literal["bbox", "mask", "poly"],
-) -> Union[InstancesType, None]:
-    insts: InstancesType = {
-        "bboxes": None,
-        "cat_ids": None,
-        "confs": None,
-        "masks": None,
-        "polys": None
-    }
-
+    label_format: Literal["bbox", "poly"],
+) -> Union[insts_lib.Instances, None]:
     if not os.path.exists(label_p):
         return None
     
     with open(label_p, "r") as f:
-        labelme_dict: LabelmeFileType = json.load(f)
+        labelme_dict: schema_lib.LabelmeFileType = json.load(f)
+    
+    if len(labelme_dict["shapes"]) == 0:
+        return None
 
+    cat_ids = []
     bboxes = []
     polys = []
 
-    for shape in labelme_dict["shapes"]:
+    for shape_dict in labelme_dict["shapes"]:
         if label_format == "bbox":
-            bboxes.append(shape["points"])
-        
+            bbox = shape_dict["points"]
+            bbox = shapes_lib.bboxLabelme2bboxSchema(bbox)
+            bboxes.append(bbox)
+        else:
+            poly = shape_dict["points"]
+            bbox = shapes_lib.polyLabelme2bboxSchema(poly)
+            poly = shapes_lib.polyLabelme2polySchema(poly)
+            bboxes.append(bbox)
+            polys.append([polys])
+
+        cat_name = shape_dict["label"]
+        cat_id = cat_name_id_dict[cat_name]
+        cat_ids.append(cat_id)
+    
+    confs = np.ones(len(cat_ids))
+    cat_ids = np.asarray(cat_ids, dtype = np.int32)
+    bboxes = np.concat(bboxes, axis = 0)
+
+    if label_format == "bbox":
+        polys = None
+
+    insts = insts_lib.Instances.from_values(
+        confs = confs, 
+        cat_ids = cat_ids,
+        bboxes = bboxes,
+        polys = polys
+    )
+
+    return insts
 
 def _load_labelme_online(
     img_root: str,
@@ -75,8 +100,8 @@ def _load_labelme_online(
     cat_name_id_dict: dict[str, int],
     label_format: Literal["bbox", "mask", "poly"],
     add_empty_img_flag: bool
-) -> OnlineDatasetType:
-    dataset_dict: OnlineDatasetType = {
+) -> schema_lib.OnlineDatasetType:
+    dataset_dict: schema_lib.OnlineDatasetType = {
         "cat_id_name_dict": {v:k for k, v in cat_name_id_dict.items()},
         "cat_name_id_dict": cat_name_id_dict,
         "format": label_format,

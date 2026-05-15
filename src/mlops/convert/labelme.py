60475d20b1fff92cd3eaae8706fbc9bb07713ed1
Liu Chang <liu67511717@126.com>
2026-05-15 16:29:13 +0800

    feat (convert/labelme.py)：WIP，更新代码

--- src/mlops/convert/labelme.py
+++ src/mlops/convert/labelme.py
@@ -10,6 +10,11 @@ import mlops.core.instances as insts_lib
 import mlops.core.schema as schema_lib
 
 
+__all__ = [
+    "load_labelme"
+]
+
+
 def _load_labelme_offline(
     img_root: str,
     labelme_root: str,
@@ -48,15 +53,23 @@ def _labelmeFile2instances(
     label_p: str,
     cat_name_id_dict: dict[str, int],
     label_format: Literal["bbox", "poly"],
+    add_empty_img_flag: bool
 ) -> Union[insts_lib.Instances, None]:
     if not os.path.exists(label_p):
-        return None
+        if not add_empty_img_flag:
+            insts = None
+        else:
+            insts = insts_lib.Instances.from_values(
+                confs = np.ones((0, ), dtype = np.float32),
+                cat_ids = np.zeros((0, ), dtype = np.int32),
+                bboxes = np.zeros((0, 4), dtype = np.int32),
+                polys = [] if label_format == "poly" else None
+            )
+
+        return insts
     
     with open(label_p, "r") as f:
         labelme_dict: schema_lib.LabelmeFileType = json.load(f)
-    
-    if len(labelme_dict["shapes"]) == 0:
-        return None
 
     cat_ids = []
     bboxes = []
@@ -77,20 +90,33 @@ def _labelmeFile2instances(
         cat_name = shape_dict["label"]
         cat_id = cat_name_id_dict[cat_name]
         cat_ids.append(cat_id)
-    
-    confs = np.ones(len(cat_ids))
-    cat_ids = np.asarray(cat_ids, dtype = np.int32)
-    bboxes = np.concat(bboxes, axis = 0)
 
-    if label_format == "bbox":
-        polys = None
+    if len(cat_ids) == 0:
+        if add_empty_img_flag:
+            confs = np.ones((0, ), dtype = np.float32),
+            cat_ids = np.zeros((0, ), dtype = np.int32),
+            bboxes = np.zeros((0, 4), dtype = np.int32),
+            polys = [] if label_format == "poly" else None
 
-    insts = insts_lib.Instances.from_values(
-        confs = confs, 
-        cat_ids = cat_ids,
-        bboxes = bboxes,
-        polys = polys
-    )
+            insts = insts_lib.Instances.from_values(
+                confs, cat_ids, bboxes, polys
+            )
+        else:
+            insts = None
+    else:
+        confs = np.ones(len(cat_ids))
+        cat_ids = np.asarray(cat_ids, dtype = np.int32)
+        bboxes = np.concat(bboxes, axis = 0)
+
+        if label_format == "bbox":
+            polys = None
+
+        insts = insts_lib.Instances.from_values(
+            confs = confs, 
+            cat_ids = cat_ids,
+            bboxes = bboxes,
+            polys = polys
+        )
 
     return insts
 
@@ -118,23 +144,37 @@ def _load_labelme_online(
             stem = Path(file).stem
             rel_dir = str(Path(img_p).relative_to(img_root).parent)
             label_p = os.path.join(labelme_root, rel_dir, f"{stem}.json")
-
-            if not os.path.exists(label_p):
-                if not add_empty_img_flag:
-                    continue
-
-            dataset_dict["img_p_list"].append(img_p)
-
-            if not os.path.exists(label_p):
-                dataset_dict["insts_list"].append(None)
             
-            with open(label_p, "r") as f:
-                labelme_dict: LabelmeFileType = json.load(f)
-                labelme_shape_dict: LabelmeShapeType = labelme_dict["shapes"]
-                bboxes = []
-                polys = []
+            insts = _labelmeFile2instances(
+                label_p, cat_name_id_dict, label_format,
+                add_empty_img_flag
+            )
 
-                if label_format == "bbox":
-                    bboxes
+            if insts is not None:
+                dataset_dict["img_p_list"].append(img_p)
+                dataset_dict["insts_list"].append(insts)
+    
+    return dataset_dict
+            
+def load_labelme(
+    img_root: str,
+    label_root: str,
+    cat_name_id_dict: dict[str, int],
+    label_format: Literal["bbox", "mask", "poly"],
+    add_empty_img_flag: bool,
+    dataset_mode: Literal["online", "offline"],
+) -> Union[schema_lib.OnlineDatasetType, schema_lib.OfflineDatasetType]:
+    if dataset_mode == "online":
+        dataset_dict = _load_labelme_online(
+            img_root, label_root, cat_name_id_dict, label_format,
+            add_empty_img_flag
+        )
+    else:
+        dataset_dict = _load_labelme_offline(
+            img_root, label_root, cat_name_id_dict, label_format,
+            add_empty_img_flag
+        )
+    
+    return dataset_dict
             
             
